c301ce50eabdc45d21992ec2f1faebd0fed703a2
nmaekawa <nmaekawa@g.harvard.edu>
2017-05-08 14:44:37 -0400

    silly tests
    - checking model-mommy

--- anno/tests/test_models.py
+++ anno/tests/test_models.py
@@ -3,18 +3,52 @@ import pdb
 import pytest
 import os
 
+
 from django.test import TestCase
 from django.test import TransactionTestCase
 
+from model_mommy import mommy
+from model_mommy.recipe import Recipe, foreign_key
+
 from anno.models import Anno, Tag, Target
+from anno.models import MEDIA_TYPES
+
 
 data_filename = os.path.join(
     os.path.abspath(os.path.dirname(__file__)), 'wa2-dev-mille.json')
 
 class AnnoTest(TransactionTestCase):
 
-
     @pytest.mark.django_db
+    def test_relationships_ok(self):
+        # create some tags
+        tags = mommy.make(Tag, _quantity=3)
+
+        # create annotations
+        anno = mommy.make(Anno)
+        anno.anno_tags = tags
+
+        # create targets
+        target = mommy.make(Target, anno=anno)
+        assert(len(anno.anno_tags.all()) == 3)
+        assert(len(anno.target_set.all()) == 1)
+
+
+    def test_target_ok(self):
+        target = mommy.make(Target)
+        assert(target.target_media in MEDIA_TYPES)
+        assert(isinstance(target.anno, Anno))
+
+
+    def test_anno_ok(self):
+        anno = mommy.make(Anno)
+        assert(isinstance(anno, Anno))
+        assert(len(anno.target_set.all()) == 0)
+        assert(anno.schema_version == 'catch_v0.1')
+
+
+
+    @pytest.mark.skip(reason='not ready to run yet')
     def test_create_anno_ok(self):
         datafile = open(data_filename, 'r')
         raw_data = datafile.read()
@@ -29,12 +63,5 @@ class AnnoTest(TransactionTestCase):
                 print('failed to create anno({})'.format(row['id']))
 
         assert len(Anno.objects.all()) > 0
-
-
-
-
-
-
-
 
 
