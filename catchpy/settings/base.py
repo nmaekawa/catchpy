5bb3664e52fa10a579c485ca17f9138b84d43435
nmaekawa <nmaekawa@g.harvard.edu>
2017-06-02 09:56:23 -0400

    add logging config

--- catchpy/settings/base.py
+++ catchpy/settings/base.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'anno.apps.AnnoConfig',
+    'catchpy_anno.apps.AnnoConfig',
     'consumer.apps.ConsumerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -127,3 +127,41 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+# Logging config
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s|%(levelname)s [%(filename)s:%(funcName)s] %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            'stream': 'ext://sys.stdout',
+        },
+        'errorfile_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'simple',
+            'filename': 'catchpy_errors.log',
+            'maxBytes': 10485760,  # 10MB
+            'backupCount': 7,
+            'encoding': 'utf8',
+        },
+    },
+    'loggers': {
+        'django': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'errorfile_handler'],
+            'propagate': True
+        },
+        'root': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    }
+}
