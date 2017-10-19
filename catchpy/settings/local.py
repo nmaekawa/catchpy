2b0af13424db77ef305a710b716d7edc43b7f9d4
nmaekawa <nmaekawa@g.harvard.edu>
2017-10-19 14:29:17 -0400

    settings dev for ec2 and vagrant; local for actually local app in host box

--- catchpy/settings/local.py
+++ catchpy/settings/local.py
@@ -1,20 +1,22 @@
 from .prod import *
 
+DEBUG = True
+
 # Django Extensions
 # http://django-extensions.readthedocs.org/en/latest/
 try:
     import django_extensions
-    #INSTALLED_APPS += ['django_extensions']
+    INSTALLED_APPS += ['django_extensions']
 except ImportError:
     pass
 
 # Django Debug Toolbar
 # http://django-debug-toolbar.readthedocs.org/en/latest/
 try:
-    #import debug_toolbar
-    #INSTALLED_APPS += ['debug_toolbar']
-    #MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
-    #DEBUG_TOOLBAR_PATCH_SETTINGS = True
+    import debug_toolbar
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+    DEBUG_TOOLBAR_PATCH_SETTINGS = True
 except ImportError:
     pass
 
