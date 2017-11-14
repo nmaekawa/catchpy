d1cd99c721c38991c3d48a044834b3e64841872d
nmaekawa <nmaekawa@g.harvard.edu>
2017-11-14 10:51:34 -0500

    include create target into update transaction

--- catchpy/__init__.py
+++ catchpy/__init__.py
@@ -1,3 +1,3 @@
 # important to use single quotes in version string
 # for post-commit tagging
-__version__ = '1.2.5'  # remove unnecessary db queries in search
+__version__ = '1.2.6'  # include create target into update transaction
