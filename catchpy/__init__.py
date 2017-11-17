ac945bd22b9a3e57b5e13951dbedaedec893b8c9
nmaekawa <nmaekawa@g.harvard.edu>
2017-11-17 11:38:48 -0500

    tag for django-admin commands; tied to provision monitoring

--- catchpy/__init__.py
+++ catchpy/__init__.py
@@ -1,3 +1,3 @@
 # important to use single quotes in version string
 # for post-commit tagging
-__version__ = '1.2.8'  # brackets in search query string for list param (catchpy api)
+__version__ = '1.3.0'  # django-admin commands to create consumer-key
