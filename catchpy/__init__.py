709bdd5ccb8296a9bfbbb16db875a828e86311da
nmaekawa <nmaekawa@g.harvard.edu>
2017-12-01 09:48:27 -0500

    tag for import/export/convert feature as django-admin cmd

--- catchpy/__init__.py
+++ catchpy/__init__.py
@@ -1,3 +1,3 @@
 # important to use single quotes in version string
 # for post-commit tagging
-__version__ = '1.3.0'  # django-admin commands to create consumer-key
+__version__ = '1.4.0'  # django-admin commands for export/import/convert
