0d8bd7f79fb572b5830c85e226c1113ad9e86161
nmaekawa <nmaekawa@g.harvard.edu>
2018-12-05 16:48:30 -0500

    admin ui: display_list for anno

--- anno/admin.py
+++ anno/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from .models import Anno, Tag, Target
 
+@admin.register(Anno)
+class AnnoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    list_display = (
+        'created', 'anno_id', 'body_format', 'creator_id',
+        'anno_reply_to', 'anno_deleted'
+    )
+
 # Register your models here.
-admin.site.register(Anno)
 admin.site.register(Tag)
 admin.site.register(Target)
