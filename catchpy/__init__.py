75237cdabbbb5d0e4dcffab68b72a94f4b73371f
nmaekawa <nmaekawa@g.harvard.edu>
2019-09-03 14:36:30 -0400

    bump pkg version 2.2.0

--- catchpy/__init__.py
+++ catchpy/__init__.py
@@ -1,3 +1,3 @@
 # important to use single quotes in version string
 # for post-commit tagging
-__version__ = '2.1.0'  # django2.3, exclude_userid search
+__version__ = '2.2.0'  # django2.2.4, more logs
