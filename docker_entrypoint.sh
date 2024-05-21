c05b9fe0313bed394ae0709204add817433743fc
Vesna Tan <tanvezi@gmail.com>
2024-05-21 17:14:22 -0400

    revert back port num from 8080 to 8000

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,6 @@
 python manage.py migrate
 python manage.py create_user --username user --password password --is_admin
 python manage.py create_consumer_pair --consumer consumer --secret secret --expire_in_weeks 520 --no-update
-python manage.py runserver 0.0.0.0:8080
+python manage.py runserver 0.0.0.0:8000
 
 
